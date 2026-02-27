16165faf152aac7e0bd69849312b772bd624c636
Deepshekar <deepshekarmu111@gmail.com>
2026-02-27 19:02:01 +0530

    Delete inventory directory

--- inventory/Labels/Plaques/itemSplit.js
+++
@@ -1,8 +0,0 @@
-const itemSplit = (item) => {
-  const inputString = item;
-  const [itemNumber, itemDescription] = inputString.split(" - ");
-
-  return [itemNumber, itemDescription];
-};
-
-module.exports = itemSplit;
